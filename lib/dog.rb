50568b435fcfbaa21b51aba5d2660c735f796dd0
Milly Flores <miflo0h@gmail.com>
2020-05-21 04:50:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
-class Dog 
-  
-  def name = (dogs_name)
+class Dog
+ 
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
-  end 
-  
-  def name 
+  end
+ 
+  def name
     @this_dogs_name
   end
 end
-
-lassie = Dog.new 
+ 
+lassie = Dog.new
 lassie.name = "Lassie"
-
+ 
 puts lassie.name
-
